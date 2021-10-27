03b667f15ba0721842fc04207129cc67d821d786
chaerin <achrvv@sookmyung.ac.kr>
2021-10-27 18:09:45 +0900

    fix: fix header logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ReactElement, useEffect, useState } from 'react'
-import { Header } from '../components/common'
 import { Global } from '@emotion/react'
 import globalStyle from 'styles/globalStyle'
-import styled from '@emotion/styled'
-import router from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  const [isLoginPage, setIsLoginPage] = useState<boolean>(false)
-
-  useEffect(() => {
-    setIsLoginPage(router.pathname === '/login')
-  }, [router.pathname])
   return (
     <div>
       <Global styles={globalStyle} />
-      {!isLoginPage && <Header />}
-      <Layout isLoginPage={isLoginPage}>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </div>
   )
 }
 export default MyApp
-
-type LayoutProps = {
-  isLoginPage: boolean
-}
-const Layout = styled.div<LayoutProps>`
-  padding-top: ${({ isLoginPage }) => (isLoginPage ? '0' : '5rem')};
-`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from 'next'
 import { ReactElement } from 'react'
+import { Header } from '@components/common'
 
 const Home: NextPage = (): ReactElement => {
-  return <></>
+  return <Header />
 }
 
 export default Home
